d80386f89e08284d5e44e8254cc5e026fd89aa23
Stefan Unertl <121638820+sunertl@users.noreply.github.com>
2024-07-05 21:55:34 -0700

    Quartz sync: Jul 5, 2024, 9:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Mostly Irrelevant",
+    pageTitle: "stefun.io",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
