911dfe3c20c18082e102b2e55684604fe30b6751
Stefan Unertl <121638820+sunertl@users.noreply.github.com>
2025-01-21 20:07:24 -0800

    Quartz sync: Jan 21, 2025, 8:07 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "lyceum",
+    pageTitle: "casual causal",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
